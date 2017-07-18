1605c99977858a5efa6f25248fd67e09dcb7fe18
dmc2015 <mclamb.donald@gmail.com>
2017-07-18 12:31:00 -0400

    got rspec working and a test to pass

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FactoryGirlTesting
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+
+    # Rails.application.configure do |config|
+    #   config.autoload_paths += %W{#{config.root}/lib}
+    # end
+
+  end
+end
