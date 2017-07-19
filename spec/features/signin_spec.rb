8122623145287777299bf1edfabce84681f77b62
dmc2015 <mclamb.donald@gmail.com>
2017-07-19 11:53:43 -0400

    added devise views, fixed the feature spec for sign in it works now

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -6,6 +6,7 @@ feature "signing in" do
   def fill_in_signin_fields
     fill_in "hacker[email]", with: hacker.email
     fill_in "hacker[password]", with: hacker.password
+    click_button "Log in"
   end
 
   scenario "visiting the site to sign in" do
